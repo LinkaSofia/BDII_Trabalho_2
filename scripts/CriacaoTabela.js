c468ffcf3bb1a31ab295dfb707703df72bf5ad8e
LinkaSofia <linkasofialunkes@gmail.com>
2023-10-23 23:32:46 -0300

    Cria tabela data no BD

---
+++ scripts/CriacaoTabela.js
@@ -0,0 +1,30 @@
+const connectToDatabase = require('./Conexao');
+const metadata = require('../Files/metadata.json');
+
+async function CriacaoTabela() {
+  try {
+    const db = await connectToDatabase();
+    const { table } = metadata;
+    await db.query(`DROP TABLE IF EXISTS DATA;`);
+    
+    const tableColumns = Object.keys(table).map((columnName) => `${columnName} INT`);
+    const createTableSQL = `
+      CREATE TABLE IF NOT EXISTS DATA (
+        ${tableColumns.join(', ')}
+      );
+    `;
+    await db.query(createTableSQL);
+
+    const columnNames = Object.keys(table).join(', ');
+    const insertInitialValuesSQL = `
+      INSERT INTO DATA (${columnNames})
+      VALUES 
+        ${table.id.map((id, index) => `(${Object.values(table).map((column) => column[index]).join(', ')})`).join(', ')}
+    `;
+
+    await db.query(insertInitialValuesSQL);
+  } catch (error) {
+    console.error('Erro ao criar a tabela e inserir os valores iniciais:', error);
+  }
+}
+module.exports = CriacaoTabela;
