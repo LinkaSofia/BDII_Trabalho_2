5940c3c53d5add70aa2ac6af4e0fedb9c52c1aca
LinkaSofia <linkasofialunkes@gmail.com>
2023-10-28 14:53:43 -0300

    cria tabela em python

---
+++ scripts/CriacaoTabela.py
@@ -0,0 +1,32 @@
+import json
+from src.conexao import conexao
+
+def cria_tabela_insere_dados():
+    try:
+        connection = conexao()
+        cursor = connection.cursor()
+        
+        with open("./src/data/metadado.json", "r") as metadata_file:
+            metadata = json.load(metadata_file)
+
+        drop_table = f"DROP TABLE IF EXISTS data"
+        cursor.execute(drop_table)
+
+        columns = [f"{col} INTEGER" for col in metadata["table"].keys()]
+        create_table = f"CREATE TABLE data ({', '.join(columns)});"
+        cursor.execute(create_table)
+
+        # Obtém os nomes das colunas da tabela.
+        column_names = ", ".join(metadata["table"].keys())
+
+        # Pega os valores do arquivo JSON e insere no banco.
+        values = ", ".join([f"({', '.join(map(str, row))})" for row in zip(*metadata["table"].values())])
+        insert_initial_values_query = f"INSERT INTO data ({column_names}) VALUES {values};"
+        cursor.execute(insert_initial_values_query)
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+    except Exception as e:
+        print("Erro: ", e)
